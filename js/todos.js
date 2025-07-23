d61e0622ea2926232ea44eb4a3f935e3d0af6d2c
ritsuto10969er <rk14oor109@gmail.com>
2025-07-24 07:12:49 +0900

    feat: Add filtering, edit cancel logic, and clear completed functionality

--- js/todos.js
+++ js/todos.js
@@ -1,12 +1,28 @@
 window.todos = function () {
     return {
+        filter: 'all',
+
         todos: [],
+
+        newTodo: '',
+
+        editedTodo: null,
         
         get active() {
             return 	this.todos.filter(todo => ! todo.completed)
         },
 
-        newTodo: '',
+        get completed() {
+            return 	this.todos.filter(todo => todo.completed)
+        },
+
+        get filteredTodos() {
+            return {
+                all: this.todos,
+                active: this.active,
+                completed: this.completed
+            }[this.filter];
+        },
 
         addTodo() {
             this.todos.push({
@@ -16,6 +32,27 @@ window.todos = function () {
             });
 
             this.newTodo = '';
+        },
+
+        editTodo(todo) {
+            todo.cacheBody = todo.body;
+            this.editedTodo = todo;
+        },
+
+        cancelEdit(todo) {
+            todo.body = todo.cacheBody;
+
+            this.editedTodo = null;
+
+            delete todo.cacheBody;
+        },
+
+        editComplete(todo) {
+            if(todo.body.trim() === '') {
+                return this.deleteTodo(todo);
+            }
+
+            this.editedTodo =null
         },
 
         deleteTodo(todo) {
